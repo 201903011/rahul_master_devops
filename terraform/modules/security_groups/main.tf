50986c3592aee3d1902a9cb8741f20f703ef2df6
Rahul Gaikwad <rahulkishorgaikwad@gmail.com>
2024-12-25 20:18:40 +0530

    chnages protocol

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "bastion" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "ALL"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -26,14 +26,14 @@ resource "aws_security_group" "private" {
   ingress {
     from_port   = 0
     to_port     = 65535
-    protocol    = "-1"
+    protocol    = "ALL"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "ALL"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -55,7 +55,7 @@ resource "aws_security_group" "web" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "ALL"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
