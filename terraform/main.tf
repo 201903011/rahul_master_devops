d5dd8d12639bd84da3a3296c7974586d183c6d07
Rahul Gaikwad <rahulkishorgaikwad@gmail.com>
2024-12-25 20:43:42 +0530

    cidr changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,10 @@ provider "aws" {
 module "vpc" {
   source                = "./modules/vpc"
   vpc_cidr              = "10.0.0.0/16"
-  private_subnet_a_cidr = "10.0.1.0/24"
-  private_subnet_b_cidr = "10.0.2.0/24"
+  public_subnet_a_cidr  = "10.0.1.0/24"
+  public_subnet_b_cidr  = "10.0.2.0/24"
+  private_subnet_a_cidr = "10.0.3.0/24"
+  private_subnet_b_cidr = "10.0.4.0/24"
 }
 
 module "security_groups" {
