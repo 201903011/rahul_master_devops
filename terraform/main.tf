af8970e90db9b24846d23684e584a9ab550b4721
Rahul Gaikwad <rahulkishorgaikwad@gmail.com>
2024-12-28 13:05:04 +0530

    my io changes revert

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ module "vpc" {
 module "security_groups" {
   source = "./modules/security_groups"
   vpc_id = module.vpc.vpc_id
-  my_ip  = "43.243.0.0/16" # my ip 
+  my_ip  = "43.243.83.0/24" # my ip 
 }
 
 module "instances" {
