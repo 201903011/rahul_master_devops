0a88cf363539fbe158f6b0e7b1df59781737dffd
Rahul Gaikwad <rahulkishorgaikwad@gmail.com>
2024-12-28 16:45:59 +0530

    change in security group

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -49,8 +49,16 @@ resource "aws_security_group" "web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = [var.my_ip]
   }
+
 
   egress {
     from_port   = 0
