9c192b4d3ba7e23006e4b974e340342f702d4afe
Rahul Gaikwad <rahulkishorgaikwad@gmail.com>
2025-01-02 22:36:37 +0530

    ingress changes

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -61,6 +61,13 @@ resource "aws_security_group" "web" {
   }
 
   ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
